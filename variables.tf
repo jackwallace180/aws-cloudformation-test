5bd7223ffd94065bc5598ecc66735e80c3957e9b
Jack Wallace <jackwallace180@hotmail.com>
2026-04-30 16:35:15 +0100

    change aws region

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "eu-west-2"
+  default     = "eu-west-1"
 }
 
 variable "instance_type" {
